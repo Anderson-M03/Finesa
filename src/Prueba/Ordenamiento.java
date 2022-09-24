1abc4b4f0dcd2cc90c39ae27e828aaad3787b05a
Anderson-M03 <andersonmon9156@gmail.com>
2022-09-23 23:24:10 -0500

    Creación Algoritmo ordenamiento

--- src/Prueba/Ordenamiento.java
+++ src/Prueba/Ordenamiento.java
@@ -1,30 +1,70 @@
 package Prueba;
 
+import javax.swing.JOptionPane;
+
 public class Ordenamiento {
 	
 	public void ordenarArray(int arreglo[], String criterioOrdenamiento) {
-		System.out.println(arreglo.length);
-		int arregloAuxiliar[] = {0,0,0};
 		
-		if(criterioOrdenamiento.equals("Menor a mayor")) {
-			for(int i = 0 ; i < arreglo.length ; i++) {
-				for (int j = 0 ; j < arregloAuxiliar.length ; j++) {
-					if(arreglo[i] > arregloAuxiliar[j]) {
-						arreglo[i] = arregloAuxiliar[j];
-					}
-				}
-				
-			}
+		if(criterioOrdenamiento.equalsIgnoreCase("Menor a mayor")) {
+			ordenarMenorMayor(arreglo);	
 		} else {
-			
+			ordenarMayorMenor(arreglo);
 		}
 	}
+	
+	public void ordenarMenorMayor(int arreglo[]) {
+		int cambios = 0;
+		boolean recorrido = true;
+		while(recorrido) {
+			for(byte i = 0 ; i < arreglo.length - 1 ; i++) {
+				if(arreglo[i] > arreglo[i+1]) {
+					int auxiliar = arreglo[i];
+					arreglo[i] = arreglo[i+1];
+					arreglo[i+1] = auxiliar;
+					cambios++;
+				}	
+			}
+			if(cambios == 0) {
+				recorrido = false;
+			}
+			cambios = 0;
+		}
+		for(byte j = 0 ; j < arreglo.length ; j++) {
+			System.out.println(arreglo[j]);
+		}
+	}
+	
+	
+	public void ordenarMayorMenor(int arreglo[]) {
+		int cambios = 0;
+		boolean recorrido = true;
+		while(recorrido) {
+			for(byte i = 0 ; i < arreglo.length - 1 ; i++) {
+				if(arreglo[i] < arreglo[i+1]) {
+					int auxiliar = arreglo[i];
+					arreglo[i] = arreglo[i+1];
+					arreglo[i+1] = auxiliar;
+					cambios++;
+				}	
+			}
+			if(cambios == 0) {
+				recorrido = false;
+			}
+			cambios = 0;
+		}
+		for(byte j = 0 ; j < arreglo.length ; j++) {
+			System.out.println(arreglo[j]);
+		}		
+	}
+
 	
 	
 	public static void main(String args[]) {
 		Ordenamiento ordenamiento = new Ordenamiento();
 		int arr[] = {6,5,4,3,2,1};
-		ordenamiento.ordenarArray(arr, "Menor a mayor");
+		String ordenar = JOptionPane.showInputDialog("Digite criterio de ordenamiento");
+		ordenamiento.ordenarArray(arr, ordenar);
 	}
 
 }
